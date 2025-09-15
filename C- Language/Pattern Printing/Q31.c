5a7968dc0befc52cd6491da178dd4a69e7b2bb70
DashratRajpurohit <dashrat.rajpurohit.cg@gmail.com>
2025-09-15 16:57:06 +0530

    Html Assignments (1-18)

--- C- Language/Pattern Printing/Q31.c
+++ C- Language/Pattern Printing/Q31.c
@@ -1,17 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    for(int i=1;i<=5; i++){
-        for(int j=1; j<=i; j++){
-            if(i==3 && j==2 || i==4 && (j==2 || j==3)){
-               printf(" ");
-            }
-            else{
+    for(int i=1; i<=5; i++){
+        for(int j=1; j<=5; j++){
+            if(i==1 || i==5 || j==1 || j==5){
                 printf("*");
             }
-            
+            else{
+                printf(" ");
+            }
         }
         printf("\n");
     }
-    return 0;
 }
--- C- Language/Pattern Printing/Q32.c
+++ C- Language/Pattern Printing/Q32.c
@@ -2,8 +2,8 @@
 int main()
 {
     for(int i=1;i<=5; i++){
-        for(int j=5; j>=i; j--){
-            if(i==3 && j==4 || i==2 && (j==4 || j==3)){
+        for(int j=1; j<=i; j++){
+            if(i==3 && j==2 || i==4 && (j==2 || j==3)){
                printf(" ");
             }
             else{

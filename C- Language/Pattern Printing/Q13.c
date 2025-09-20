883d1719cc070ea3f2670b5bd0fa34d2b50dc2d6
DashratRajpurohit <dashrat.rajpurohit.cg@gmail.com>
2025-09-20 22:42:09 +0530

    Pattern printing Q51-60

--- C- Language/Pattern Printing/Q13.c
+++ C- Language/Pattern Printing/Q13.c
@@ -3,7 +3,7 @@
 int main()
 {
    for (int i=1; i<=5; i++){
-     for(int j=4;j>=i; j--){
+     for(int j=5;j>  i; j--){
         printf(" ");
     }   
         
--- C- Language/Pattern Printing/Q53.c
+++ C- Language/Pattern Printing/Q53.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
 int main()
 {
-    int count=1;
-    for(int i=1;i<=5;i++){
-        for(int j=1;j<=5;j++){
+    int count=0;
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=4+i-1;j++){
             
-            if(i+j<=5){
-                printf("0");
+            if(i+j<=4){
+                printf("  ");
             }
             else{
+                 count++;
                 printf("%d",count);
+                if(count<10){
+               printf(" ");
+                }
             }
         }
         
